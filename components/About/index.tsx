c28e07541605d3a0e09b091d682bb7944a8f028d
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-28 17:51:51 +0530

    added

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -91,7 +91,7 @@ const About = () => {
         <img src="./images/shape/check-circle.png" alt="Tick Icon" className="w-5 h-5" />
       </div>
       <div className="text-black text-base font-normal font-['Poppins'] leading-relaxed md:whitespace-nowrap">
-        Experienced Designers
+      Quality Content
       </div>
     </div>
 
@@ -100,7 +100,7 @@ const About = () => {
         <img src="./images/shape/check-circle.png" alt="Tick Icon" className="w-5 h-5" />
       </div>
       <div className="text-black text-base font-normal font-['Poppins'] leading-relaxed md:whitespace-nowrap">
-      Quality Content
+     Experienced Designers
       </div>
     </div>
   </div>
