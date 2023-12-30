5d07a5c2514d6b94764b8e7d571789b4810eebaf
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-30 13:42:49 +0530

    added data

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -29,49 +29,7 @@ const menuData: Menu[] = [
     newTab: false,
     path: "/team",
   },
-  {
-    id: 3,
-    title: "Page links",
-    newTab: false,
-    submenu: [
-      {
-        id: 31,
-        title: "Atoms Group",
-        newTab: false,
-        path: "https://atoms-group2.vercel.app/",
-      },
-      {
-        id: 34,
-        title: "Atoms Education",
-        newTab: false,
-        path: "https://atoms-education-omega.vercel.app/",
-      },
-      {
-        id: 35,
-        title: "Atoms Digital Solutins ",
-        newTab: false,
-        path: "https://digital-solutions-orpin.vercel.app/",
-      },
-      {
-        id: 35,
-        title: "Atoms 2.O",
-        newTab: false,
-        path: "http://atoms2-0.vercel.app",
-      },
-      // {
-      //   id: 35.1,
-      //   title: "Support",
-      //   newTab: false,
-      //   path: "/support",
-      // },
-      {
-        id: 36,
-        title: "",
-        newTab: false,
-        path: "/error",
-      },
-    ],
-  },
+  
 ];
 
 export default menuData;
