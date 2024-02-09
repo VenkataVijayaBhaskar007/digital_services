635c48473bb454a0c9272ebfe4acd199a7e14a98
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2024-02-09 11:56:32 +0530

    added acumen club link

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -110,7 +110,7 @@ export default function Footer() {
                     <Link href="https://digitalmarketingservices.atomsgroup.net/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Digital Marketing Services</Link>
                   </li>
                   <li className="mb-1">
-                    <Link href="https://softwareservices.atomsgroup.net/" className="text-gray-300 hover:text-gray-100 transition duration-150 ease-in-out">Software Services</Link>
+                    <Link href="https://softwaresolutions.atomsgroup.net/" className="text-gray-300 hover:text-gray-100 transition duration-150 ease-in-out">Software Solutions</Link>
                   </li>
                   <li className="mb-1">
                     <Link href="https://visualmedia.atomsgroup.net/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Visual Media</Link>
@@ -134,7 +134,7 @@ export default function Footer() {
                     <Link href="https://education.atomsgroup.net/section2" className="text-gray-300 hover:text-gray-100 transition duration-150 ease-in-out">The Acumen Club</Link>
                   </li>
                   <li className="mb-1">
-                    <Link href="https://education.atomsgroup.net/section3" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Atoms Tutions</Link>
+                    <Link href="https://education.atomsgroup.net//TheAcumenClub" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Atoms Tutions</Link>
                   </li>
                   <li className="mb-1">
                     <Link href="https://education.atomsgroup.net/section4" className="text-gray-300 hover:text-gray-100 transition duration-150 ease-in-out">Atoms Summer Camps</Link>
